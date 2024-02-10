56a50498640bb365ebc41a043f07814e5d2f1f08
Aby0206 <t.abyantonyd@gmail.com>
2024-02-10 16:53:03 +0530

    padding for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { styles } from "../styles";
 // import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-  return (
+    return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
@@ -30,8 +30,8 @@ const Hero = () => {
         </div>
 
         {/* <ComputersCanvas /> */}
-
-        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center mt-10 sm:mt-0">
           <a href="#about">
             <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
               <motion.div
