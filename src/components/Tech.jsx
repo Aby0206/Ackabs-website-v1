a66927cdc1524d4129b4a08557369137356fae24
Aby0206 <t.abyantonyd@gmail.com>
2024-02-10 18:46:28 +0530

    error handling

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,15 @@
 import { BallCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
-
+import ErrorBoundary from './ErrorBoundary';
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology)=>(
         <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-
+          <ErrorBoundary>
+            <BallCanvas icon={technology.icon} />
+          </ErrorBoundary>
         </div>
       ))}
 
