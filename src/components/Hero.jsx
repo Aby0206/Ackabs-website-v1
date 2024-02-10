1f62a51680a9ed73b222f78cd7ecbc2232441a78
Aby0206 <t.abyantonyd@gmail.com>
2024-02-10 17:12:56 +0530

    remove margin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,13 @@ import { styles } from "../styles";
 // import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-    return (
+  const style_mob = {
+    padding: '20px',
+    '@media (maxWidth: 768px)':{
+      paddingTop:'50px',
+    }
+  };
+  return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
@@ -31,7 +37,7 @@ const Hero = () => {
 
         {/* <ComputersCanvas /> */}
         
-        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center mt-10 sm:mt-0">
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
           <a href="#about">
             <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
               <motion.div
