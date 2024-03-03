f0c8f1033bef98880f74214766d2137ce03d4352
Aby0206 <t.abyantonyd@gmail.com>
2024-03-04 02:07:40 +0530

    center side fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 // import { ComputersCanvas } from "./canvas";
 import stylescss from './styles.module.css';
+import './total.css';
 const Hero = () => {
   
   return (
@@ -32,7 +33,7 @@ const Hero = () => {
 
         {/* <ComputersCanvas /> */}
         
-        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center centertotalme">
           <a href="#about">
             <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
               <motion.div
